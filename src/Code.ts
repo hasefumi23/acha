7b6f3b079ada084d287625743365cc17ec7b0ac3
hasefumi23 <hasefumi23@gmail.com>
2018-12-03 00:23:34 +0900

    init clasp with typescript

---
+++ src/Code.ts
@@ -0,0 +1,119 @@
+const _ = LodashGS.load();
+import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
+
+const HIKKI_URL = "HIKKI_URL";
+const SLACK_POST_URL = "SLACK_POST_URL";
+const CALENDAR_ID = "CALENDAR_ID";
+const SECRET = "my_secret";
+const EXCEPT_MEDIA = ["SPACE SHOWER TV Plus", "SPACE SHOWER TV", "MUSIC ON! TV", "MTV"];
+const REMINDER_BEFORE_TIME = 15;
+
+/**
+ * Google Spread Sheetの場合、セルにデータを保存する際に勝手にデータの方を推測されて変な型で保存されるので、signatureをとっておく。
+ * replaceしているのは、Spread Sheet内で[=, +]は式の一部とみなされてしまうため。
+ *
+ * @param {*} item
+ * @returns
+ */
+const createSignature = (item: any) => {
+  const message = [item.date, item.startTime, item.endTime, item.media, item.program].join();
+  return Utilities.base64Encode(
+    Utilities.computeHmacSignature(
+      Utilities.MacAlgorithm.HMAC_SHA_512,
+      message,
+      SECRET,
+    ),
+  ).replace(/=/g, "E").replace(/\+/g, "P");
+};
+
+const postSlack = (message: string) => {
+  // TODO: ちゃんと画像とかを用意する
+  const payload = {
+    channel: "#hikki",
+    icon_emoji: ":ghost:",
+    text: message,
+    username: "hikki info",
+  };
+  const options: URLFetchRequestOptions = {
+    contentType: "application/json",
+    method: "post",
+    payload: JSON.stringify(payload),
+  };
+
+  UrlFetchApp.fetch(SLACK_POST_URL, options);
+};
+
+const createGcalendar = (title: string, media: string, date: string, startTime: string, endTime: string, description: string) => {
+  // TODO: 予定にラベルを付けたい
+  const event = CalendarApp.getCalendarById(CALENDAR_ID);
+  const startAt = new Date(date + " " + startTime);
+  let endAt = null;
+  if (_.isEmpty(endTime)) {
+    // たまにendTimeに値が入ってこない場合があるので手当する
+    endAt = new Date(date + " " + startTime);
+    endAt.setHours(startAt.getHours() + 1);
+  } else {
+    endAt = new Date(date + " " + endTime);
+  }
+
+  event.createEvent("[" + media + "]" + title, startAt, endAt, { description })
+  event.createEvent(`[${media}]${title}`, startAt, endAt, { description })
+    .addPopupReminder(REMINDER_BEFORE_TIME);
+};
+
+const fetchUtadaJson = () => {
+  const context = UrlFetchApp.fetch(HIKKI_URL).getContentText();
+  // TODO: callbackをreplaceする処理を不要にできるはず
+  const matched = context.match(/callback\(({.*})\)/) || "";
+  const json = JSON.parse(matched[1].toString());
+
+  return json;
+};
+
+export default function main() {
+  const logTime = new Date();
+  Logger.log("[" + logTime.toString() + "]" + "START");
+
+  const utadaJson = fetchUtadaJson();
+  const utadaTvItems = utadaJson.items.tv.reverse();
+
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("TV_INFO_LIST");
+  const lastRow = sheet.getLastRow();
+
+  // データがない場合(初実行時のみの想定)は、データを突っ込んで処理を終了する
+  if (lastRow === 0) {
+    utadaTvItems.forEach((item: any) => {
+      const signature = createSignature(item);
+      sheet.appendRow([signature, item.date, item.startTime, item.endTime, item.media, item.program]);
+    });
+
+    return;
+  }
+
+  // 最新の行のsignatureを取得する
+  const lastSignature = sheet.getSheetValues(lastRow, 1, 1, 4)[0][0];
+
+  // fetchしてきたデータの配列でsignatureが一致するindexを探す
+  const index = _.findIndex(utadaTvItems, (item: any) => {
+    const signature = createSignature(item);
+    return lastSignature === signature;
+  });
+
+  // まだ取り込んでいないデータに対してのみ各処理を実施する
+  utadaTvItems.slice(index + 1).forEach((item: any) => {
+    const signature = createSignature(item);
+    sheet.appendRow([signature, item.date, item.startTime, item.endTime, item.media, item.program]);
+    const message = "Hi @hasefumi23\n" +
+      "I'll be on this TV program. Check it out!\n" +
+      `_${item.date} (${item.startTime} - ${item.endTime})_\n` +
+      `*${item.program}*\n` +
+      `${item.note.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")}\n`;
+
+    postSlack(message);
+
+    if (!_.includes(EXCEPT_MEDIA, item.media)) {
+      // 地デジで放送されているもののみカレンダーに登録したい
+      createGcalendar(item.program, item.media, item.date.replace(/\./g, "/"), item.startTime, item.endTime, item.note);
+    }
+  });
+}
---
+++ test/Code.test.ts
@@ -0,0 +1,14 @@
+// import assert from "power-assert";
+// import Acha from "../src/Code";
+
+// describe("Test", () => {
+//   const test = new Acha("test");
+
+//   it("should have a name", () => {
+//     assert.equal(test.name, "test");
+//   });
+
+//   it("should return hello", () => {
+//     assert.equal(test.greeting("script"), "Hello, script");
+//   });
+// });
